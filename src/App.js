6c4bb08c1cb3a4f76d943221b018630e3af7d5b4
Adnan Khan <adnan7.zain@gmail.com>
2021-11-19 17:47:10 +0500

    display the data in slider

--- src/App.js
+++ src/App.js
@@ -1,15 +1,63 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import data from './data';
+import { FaQuoteRight } from 'react-icons/fa';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
 function App() {
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [people, index]);
+
   return (
     <main>
       <section className='section'>
         <div className='title'>
           <h2>
             <span>/</span>
-            hellow word
+            reviews
           </h2>
+        </div>
+
+        <div className='section-center'>
+          {people.map((person, personIndex) => {
+            const { id, image, title, quote, name } = person;
+            // something here
+            let position = 'anextSlide';
+            if (personIndex === index) {
+              position = 'activeSlide';
+            }
+            if (
+              personIndex === index - 1 ||
+              (index === 0 && personIndex === people.length - 1)
+            ) {
+              position = 'lastSlide';
+            }
+            return (
+              <article key={id} className={position}>
+                <img src={image} alt={title} className='person-img' />
+                <h4> {name}</h4>
+                <p className='title'>{title}</p>
+                <p className='text'>{quote}</p>
+                <FaQuoteRight className='icon' />
+              </article>
+            );
+          })}
+          <div className='prev' onClick={() => setIndex(index - 1)}>
+            <FiChevronLeft />
+          </div>
+          <div className='next' onClick={() => setIndex(index + 1)}>
+            <FiChevronRight />
+          </div>
         </div>
       </section>
     </main>
