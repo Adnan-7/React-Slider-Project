06217c1c5b4896b99a4d6faab39d70b99bc0f43a
Adnan Khan <adnan7.zain@gmail.com>
2021-11-19 12:44:01 +0500

    Slider Project Setup

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <section className='section'>
+        <div className='title'>
+          <h2>
+            <span>/</span>
+            hellow word
+          </h2>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default App;
